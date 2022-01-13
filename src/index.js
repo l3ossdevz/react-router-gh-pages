4fd80caf9069fb2dc6c5136cb626ff4c9c2a6bfa
Kritsada Tariya <mr.kritsada.t@gmail.com>
2022-01-13 19:16:03 +0700

    fix 404 on gh pages

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV !== 'development') {
 
 ReactDOM.render(
     <React.StrictMode>
-        <HashRouter basename={'/react-router-gh-pages'}>
+        <HashRouter>
             <App />
         </HashRouter>
     </React.StrictMode>,
